a7cfa2ba9428e90ca3bcf976b8cced2670f02744
Weslen Almeida <weslen-a@hotmail.com>
2020-09-17 13:38:55 -0300

    pushing course spring to github

---
+++ demo-ajax/src/test/java/com/course/demoajax/DemoAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.demoajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
